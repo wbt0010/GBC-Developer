79b933fd93dfbc45b98ed6c2c4fd340fa06694d2
Willyrd Rutherford Trautman III <42623915+wbt0010@users.noreply.github.com>
2020-03-06 12:32:10 -0500

    Delete make.bat

--- make.bat
+++
@@ -1,8 +0,0 @@
-C:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -c -o dia.o dia.c
-C:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j  -c -o sprite_run.o sprite_run.c
-c:\gbdk\bin\lcc -Wa-l -Wl-m -Wl-j -Wl-yp0x143=0x80 -o gbcsprites.gbc dia.o sprite_run.o
-
-DEL *.lst
-DEL *.o
-DEL *.map
-DEL *.sym
