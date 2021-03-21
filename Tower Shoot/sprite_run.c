104b71f6dc69312e27f6c75905146dde514189e6
Willyrd Rutherford Trautman III <42623915+wbt0010@users.noreply.github.com>
2021-03-20 21:07:43 -0400

    Update sprite_run.c

--- Tower Shoot/sprite_run.c
+++ Tower Shoot/sprite_run.c
@@ -3,13 +3,17 @@
 #include <stdio.h>
 //Name of file with sprite martices
 #include "dia.h"
-
-UINT8 posshift = 0;
+UINT8 status = 0;
 UINT8 mappos = 0;
-UINT8 playerlocation[2];
-UINT8 aimdire = 0;
-UINT8 moveplayerx = 0;
-UINT8 moveplayery = 0;
+UINT8 spawn = 0;
+UINT8 spawnlocal = 0;
+UINT8 enemyspawn = 0;
+UINT8 crosshairlocation[2];
+UINT8 selectorlocation[2];
+UINT8 towerAlocation[2];
+UINT8 towerBlocation[2];
+UINT8 towerClocation[2];
+UINT8 outofthefuckingway[2];
 
 const UWORD backgpalette[] = {
     RGB_DARKGRAY, RGB_DARKGRAY, RGB_DARKGRAY, RGB_DARKGRAY
@@ -21,7 +25,7 @@ const UWORD spritepalette[] = {
     diaCGBPal0c2,
     diaCGBPal0c3,
 };
-
+//delay function so the game doesn't run too fast
 void performantdelay(UINT8 numloops){
     UINT8 i;
     for(i = 0; i < numloops; i++){
@@ -30,184 +34,92 @@ void performantdelay(UINT8 numloops){
 }
 
 void main(){
-
-    playerlocation[0] = 88;
-    playerlocation[1] = 120;
+    //tower sprite location [0],x [1],y
+    towerAlocation[0] = 80;
+    towerAlocation[1] = 108;
+    towerBlocation[0] = 40;
+    towerBlocation[1] = 72;
+    towerClocation[0] = 120;
+    towerClocation[1] = 72;
+    //initializing selector location
+    selectorlocation[0] = towerAlocation[0];
+    selectorlocation[1] = towerAlocation[1];
+    crosshairlocation[0] = 80;
+    crosshairlocation[1] = 72;
     set_bkg_palette(0,1,&backgpalette[0]);
     set_sprite_palette(0,1,&spritepalette[0]);
     
     //(First Sprite tile desired, Final Sprite tile desired, VariableXY name)
-    set_sprite_data(0, 6, dia);
-    //(0 does nothing, Number Tile desired to be displayed)
-    set_sprite_tile(0, 0);
+    set_sprite_data(0, 3, dia);
+    //(Entity #, Number Tile desired to be displayed)
+    //selector tile(tile identifyer, tile location)
+    set_sprite_tile(0,0);
+    set_sprite_tile(1,1);
+    set_sprite_tile(2,2);
     set_sprite_prop(0,0);
-    //Initial Location of sprite (0 does nothing, X, Y)
-    move_sprite(0, playerlocation[0], playerlocation[1]);
 
     SHOW_SPRITES;
 
     while (1){
-
-        //At Point A
-        if(mappos == 0){
-            
-            //Move to Point B
-            if (joypad() & J_B && joypad() & J_LEFT){
-                posshift = 1;
-            }
-            while (posshift == 1){
-                performantdelay(3);
-                moveplayerx = -2;
-                moveplayery = -1;
-                scroll_sprite ( 0, moveplayerx, moveplayery);
-                playerlocation[0] -= 2;
-                 if (playerlocation[0] <= 48){
-                    posshift = 0;
-                    mappos = 1;
-                    playerlocation[0] = 48;
-                // move_sprite (0, 48, 88);
-                    }
-                }
-            //Move toward Point C
-            if (joypad() & J_B && joypad() & J_RIGHT){
-                posshift = 2;
-            }
-            while (posshift == 2){
-                performantdelay(3);
-                moveplayerx = 2;
-                moveplayery = -1;
-                scroll_sprite ( 0, moveplayerx, moveplayery);
-                playerlocation[0] += 2;
-                if (playerlocation[0] >= 128){
-                    posshift = 0;
-                    mappos = -1;
-                    playerlocation[0] = 128;   
-                    //move_sprite (0, 128, 88);                
-                }
-            }
-                
-            
-            if(joypad() & J_LEFT){
-                set_sprite_tile(0, 2);
-                aimdire = 2;
-            }
-
-            if(joypad() & J_RIGHT){
-                set_sprite_tile(0, 1);
-                aimdire = 1;
-            }
-
-            if(joypad() & J_UP){
-                set_sprite_tile(0, 0);
-                aimdire = 0;
-                
-            }
-
-            if(joypad() & J_DOWN){
-                //nothing for now
-            }
-
-            if(joypad() & J_A){
-                //firing button
-            }
-
-            if(joypad() & J_B){
-                //nothing for now
+        if(status == 0){
+        if(joypad() & J_DOWN){
+            selectorlocation[0] = towerAlocation[0];
+            selectorlocation[1] = towerAlocation[1];
+            mappos = 0;
+        }
+        if(joypad() & J_LEFT){
+            selectorlocation[0] = towerBlocation[0];
+            selectorlocation[1] = towerBlocation[1];
+            mappos = 1;
+        }
+        if(joypad() & J_RIGHT){
+            selectorlocation[0] = towerClocation[0];
+            selectorlocation[1] = towerClocation[1];
+            mappos = 2;
             }
         }
-
-        //At Point B
-        if(mappos == 1){
-            
-            //Move to Point A
-            if (joypad() & J_B && joypad() & J_RIGHT){
-                posshift = 2;
+        while(joypad() & J_B){
+            status = 1;
+            move_sprite(0, outofthefuckingway[0], outofthefuckingway[1]);
+            if(joypad() & J_UP && crosshairlocation[1] >= 20){
+                crosshairlocation[1] = crosshairlocation[1] - 1;
             }
-            while (posshift == 2){
-                performantdelay(3);
-                moveplayerx = 2;
-                moveplayery = 1;
-                scroll_sprite ( 0, moveplayerx, moveplayery);
-                playerlocation[0] += 2;
-                if (playerlocation[0] >= 88){
-                    posshift = 0;
-                    mappos = 0;
-                    playerlocation[0] = 88;   
-                    //move_sprite (0, 128, 88);
-                }
+            if(joypad() & J_DOWN && crosshairlocation[1] <= towerAlocation[1]){
+                crosshairlocation[1] = crosshairlocation[1] + 1;
             }
-            if(joypad() & J_LEFT){
-                //nothing for now
-            }
-
-            if(joypad() & J_RIGHT){
-                set_sprite_tile(0, 3);
-                aimdire = 3;
-            }
-
-            if(joypad() & J_UP){
-                set_sprite_tile(0, 5);
-                aimdire = 5;
-            }
-
-            if(joypad() & J_DOWN){
-                set_sprite_tile(0, 4);
-                aimdire = 4;
-            }
-
-            if(joypad() & J_A){
+            if(joypad() & J_LEFT && crosshairlocation[0] >= towerBlocation[0]){
+                crosshairlocation[0] = crosshairlocation[0] - 1;
+             }
+            if(joypad() & J_RIGHT && crosshairlocation[0] <= towerClocation[0]){
+                crosshairlocation[0] = crosshairlocation[0] + 1;
                 
-            }
-
-            if(joypad() & J_B){
-                
-            }
+             }
+            move_sprite(1, crosshairlocation[0], crosshairlocation[1]);
+            delay(8);
         }
-
-        //At Point C
-        if(mappos == -1){
-            
-            //Move to Point A
-            if (joypad() & J_B && joypad() & J_LEFT){
-                posshift = 1;
-            }
-            while (posshift == 1){
-                performantdelay(3);
-                moveplayerx = -2;
-                moveplayery = 1;
-                scroll_sprite ( 0, moveplayerx, moveplayery);
-                playerlocation[0] -= 2;
-                 if (playerlocation[0] <= 88){
-                    posshift = 0;
-                    mappos = 0;
-                    playerlocation[0] = 88;
-                }
-            }
-            if(joypad() & J_LEFT){
-                
-            }
-
-            if(joypad() & J_RIGHT){
-                
-            }
-
-            if(joypad() & J_UP){
-                
-            }
-
-            if(joypad() & J_DOWN){
-                
-            }
-
-            if(joypad() & J_A){
-                
-            }
-
-            if(joypad() & J_B){
-                
-            }
+        status = 0;
+        move_sprite(0, selectorlocation[0], selectorlocation[1]);
+        move_sprite(1, outofthefuckingway[0], outofthefuckingway[1]);
+        spawn += 1;
+        if(spawn <= 50){
+            spawnlocal = 0;
+            enemyspawn = 35;
+         }
+        if(spawn >= 51 && spawn <= 101){
+            spawnlocal = 1;
+            enemyspawn = 70;
         }
-        performantdelay(4);
+        if(spawn >= 102 && spawn <= 152){
+            spawnlocal = 2;
+            enemyspawn = 105;
+         }
+         if(spawn >= 204 && spawn <= 255){
+            spawnlocal = 3;
+            enemyspawn = 140;
+         }
+         if(spawnlocal == 0){
+             move_sprite(2, enemyspawn, 50);
+         }
+         
     }
-
-}
+} 
