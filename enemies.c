029646937fb093cdec2d76ab6d0a1fca39c4105b
Willyrd Rutherford Trautman III <42623915+wbt0010@users.noreply.github.com>
2021-03-21 19:04:34 -0400

    Add files via upload

---
+++ enemies.c
@@ -0,0 +1,42 @@
+/*
+#include <gb/gb.h>
+#include <gb/cgb.h>
+
+UINT8 spawn = 0;
+UINT8 spawnlocal = 0;
+UINT8 enemyspawn[2];
+
+void enemies{
+enemyspawn[0] = 80;
+enemyspawn[1] = 0;
+if(enemyspawn[1] <= 70){
+enemyspawn[1] += enemyspawn[1];
+    }
+}
+        if(spawn <= 50){
+            spawnlocal = 0;
+            enemyspawn = 35;
+         }
+        if(spawn >= 51 && spawn <= 101){
+            spawnlocal = 1;
+            enemyspawn = 70;
+        }
+        if(spawn >= 102 && spawn <= 152){
+            spawnlocal = 2;
+            enemyspawn = 105;
+         }
+         if(spawn >= 204 && spawn <= 255){
+            spawnlocal = 3;
+            enemyspawn = 140;
+         }
+        if(spawnlocal == 1){
+             move_sprite(2, enemyspawn, 50);
+         }
+         if(spawnlocal == 2){
+             move_sprite(2, enemyspawn, 50);
+         }
+         if(spawnlocal == 3){
+             move_sprite(2, enemyspawn, 50);
+         }
+         */
+         
